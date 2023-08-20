d4d0cac6d53eb54093f74c66559b126b75d21a6e
Joseph Webster <rwc.webster@gmail.com>
2023-08-20 08:26:53 +0100

    Attempt to fix the execution of code

--- static/js/script.js
+++ static/js/script.js
@@ -1,59 +1,59 @@
-// Get references to DOM elements
-const languageSelect = document.getElementById('language-select');
-const codeEditor = monaco.editor.create(document.getElementById('editor'), {
-  value: getDefaultCode(),
-  language: 'python', // Set the default language to Python
-});
-
-// Mapping of programming languages to their respective default code
-const defaultCodeMap = {
-  'python': `print("Hello, World!")`,
-  'javascript': `console.log("Hello, World!");`,
-  'java': `public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+require.config({
+    paths: {
+        'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.40.0/min/vs'
     }
-}`,
-};
-
-// Initialize the Monaco editor with default code
-function getDefaultCode() {
-  const defaultLanguage = 'python'; // Set the default language here
-  return defaultCodeMap[defaultLanguage];
-}
-
-// Update the code editor when a new language is selected
-languageSelect.addEventListener('change', () => {
-  const selectedLanguage = languageSelect.value;
-  codeEditor.setValue(defaultCodeMap[selectedLanguage] || '');
-  codeEditor.setModelLanguage(codeEditor.getModel(), selectedLanguage);
 });
 
-// Function to run the code and display output
-async function runCode() {
-  const code = codeEditor.getValue();
-  const selectedLanguage = languageSelect.value;
-
-  // Make a POST request to the server to run the code
-  try {
-    const response = await fetch('/run', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        code: code,
-        language: selectedLanguage,
-      }),
+require(['vs/editor/editor.main'], function () {
+    const editor = monaco.editor.create(document.getElementById('editor-container'), {
+        value: '',
+        language: 'python',
+        theme: 'vs-dark'
     });
 
-    if (response.ok) {
-      const result = await response.text();
-      document.getElementById('output').textContent = result;
-    } else {
-      console.error('Error running code:', response.statusText);
+    const languageSelect = document.getElementById('language-select');
+    const runButton = document.getElementById('run-button');
+    const outputContainer = document.getElementById('output-container');
+
+    languageSelect.addEventListener('change', function () {
+        const selectedLanguage = languageSelect.value;
+        const helloWorldCode = getHelloWorldCode(selectedLanguage);
+        editor.setValue(helloWorldCode);
+        monaco.editor.setModelLanguage(editor.getModel(), selectedLanguage);
+    });
+
+    runButton.addEventListener('click', function () {
+        const code = editor.getValue();
+        const selectedLanguage = languageSelect.value;
+        
+        // Send code to server for execution
+        fetch('/run', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ code, language: selectedLanguage }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Display the output in the outputContainer
+            outputContainer.innerText = `Output:\n${data.output}`;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    });
+
+    function getHelloWorldCode(language) {
+        switch (language) {
+            case 'python':
+                return 'print("Hello, World!")';
+            case 'java':
+                return 'class HelloWorld {\n    public static void main(String[] args) {\n        System.out.println("Hello, World!");\n    }\n}';
+            case 'javascript':
+                return 'console.log("Hello, World!");';
+            default:
+                return '';
+        }
     }
-  } catch (error) {
-    console.error('Error:', error);
-  }
-}
+});
